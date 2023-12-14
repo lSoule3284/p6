446e16ec4cb03425cfdcc9e9e83bec1c69948126
Efe Caputlu <98910603+efecaputlu@users.noreply.github.com>
2023-12-13 22:32:44 -0600

    Update delete.C

--- delete.C
+++ delete.C
@@ -21,9 +21,9 @@ const Status QU_Delete(const string & relation,
     RID recordID;
     int countOfDeletedTuples = 0;
     HeapFileScan scan(relation, executeStatus);
-    
+
     if (executeStatus != OK) {
-        return executeStatus;
+        return executeStatus; 
         }
 
     AttrDesc descriptor;
@@ -32,54 +32,51 @@ const Status QU_Delete(const string & relation,
     if(attrName.empty()) {
         executeStatus = scan.startScan(0, 0, STRING, NULL, EQ);
         if (executeStatus != OK) {
-            return executeStatus;
+            return executeStatus; 
             }
         while (scan.scanNext(recordID) == OK) {
             executeStatus = scan.deleteRecord();
-            if (executeStatus != OK) { 
-            return executeStatus; 
-            }
+            if (executeStatus != OK) {
+                return executeStatus;
+                }
             countOfDeletedTuples++;
         }
         return OK;
     }
 
     executeStatus = attrCat->getInfo(relation, attrName, descriptor);
-    
-    if (executeStatus != OK) { 
+    if (executeStatus != OK) {
         return executeStatus; 
         }
+
+    int intValue = atoi(attrValue);
+    float floatValue = atof(attrValue);
 
     switch (type) {
         case STRING:
             comparisonValue = attrValue;
             break;
-        case INTEGER: {
-            int intValue = *(reinterpret_cast<const int*>(attrValue));
-            comparisonValue = reinterpret_cast<const char*>(&intValue);
+        case INTEGER:
+            comparisonValue = (char*)&intValue;
             break;
-        }
-        case FLOAT: {
-            float floatValue = *(reinterpret_cast<const float*>(attrValue));
-            comparisonValue = reinterpret_cast<const char*>(&floatValue);
+        case FLOAT:
+            comparisonValue = (char*)&floatValue;
             break;
-        }
     }
 
     executeStatus = scan.startScan(descriptor.attrOffset, descriptor.attrLen, type, comparisonValue, op);
     
-    if (executeStatus != OK) { 
-        return executeStatus; 
+    if (executeStatus != OK) {
+        return executeStatus;
         }
     while (scan.scanNext(recordID) == OK) {
         executeStatus = scan.deleteRecord();
-        if (executeStatus != OK) { 
-            return executeStatus; 
-        }
+        if (executeStatus != OK) {
+            return executeStatus;
+            }
+
         countOfDeletedTuples++;
     }
 
     return OK;
 }
-
-
